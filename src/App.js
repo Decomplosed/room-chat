46dc8456379ff6d46da2a552e7d1ec4836c18513
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-14 09:43:15 +0100

    passed projectID as env variable

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 const App = () => {
-  return <ChatEngine height='100vh' projectID='' userName='' userSecret='' />;
+  return (
+    <ChatEngine
+      height='100vh'
+      projectID={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
+      userName=''
+      userSecret=''
+    />
+  );
 };
 
 export default App;
