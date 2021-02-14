e7f576705ad3f4bce6a0b03cce83bea58a961b49
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-14 12:04:49 +0100

    imported all components related to ChatFeed

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
 
 const ChatFeed = (props) => {
   console.log(props);
