e8cff53bf4da5c74d2d706f5479c325f01e8ef58
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-15 08:35:37 +0100

    added props as parameters

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -3,7 +3,7 @@ import MessageForm from './MessageForm';
 import MyMessage from './MyMessage';
 import TheirMessage from './TheirMessage';
 
-const ChatFeed = ({ chats, activeChat, userName, messages }) => {
+const ChatFeed = (props, { chats, activeChat, userName, messages }) => {
   const chat = chats && chats[activeChat];
 
   const renderMessages = () => {
