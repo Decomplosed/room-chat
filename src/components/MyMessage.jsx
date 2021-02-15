1d1a9cb3e0f0fc3481afe1773995a8f6e5aa42ad
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-15 08:39:04 +0100

    setup template if the message is an image

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 const MyMessage = ({ message }) => {
+  if (message?.attachments?.length > 0) {
+    return (
+      <img
+        src={message.attachments[0].file}
+        alt='message-attachment'
+        className='message-image'
+        style={{ flaot: 'right' }}
+      />
+    );
+  }
+
   return <div>MyMessage</div>;
 };
 
