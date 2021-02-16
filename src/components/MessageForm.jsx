cebfaaba61eb291637b0e2dde94a26691a5f7737
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-16 23:11:25 +0100

    if there are any characters inside text, send a message

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -8,6 +8,8 @@ const MessageForm = (props, { chatId, creds }) => {
     event.preventDefault();
 
     const text = value.trim();
+
+    if (text.length > 0) sendMessage(creds, chatId, { text });
   };
 
   const handleChange = (event) => {
