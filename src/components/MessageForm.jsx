f3683dbc69a294f85cc2b6dc9fd00daf216ad77b
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-16 23:02:35 +0100

    added useState for value variable

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 const MessageForm = () => {
+  const [value, setValue] = useState('');
+
   const handleSubmit = () => {};
 
   const handleChange = () => {};
