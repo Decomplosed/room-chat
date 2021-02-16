368005600d33edcb9c84304aeb8efeba3313e379
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-16 23:00:50 +0100

    added styles to input

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const MessageForm = () => {
   const handleSubmit = () => {};
 
-  return <form className='message-form' onSubmit={handleSubmit}>
-    <input />
-  </form>;
+  return (
+    <form className='message-form' onSubmit={handleSubmit}>
+      <input className='message-input' />
+    </form>
+  );
 };
 
 export default MessageForm;
