bf803a8d98b96d3b4bff895682e8d8d282727e3b
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-19 09:47:43 +0100

    added containers for form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,4 +4,10 @@ import axios from 'axios';
 const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPaddword] = useState('');
+
+  return (
+    <div className='wrapper'>
+      <div className='form'></div>
+    </div>
+  );
 };
