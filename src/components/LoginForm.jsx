29744a93fcda8da027510d688580feff9628b27a
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-19 09:48:20 +0100

    added Chat Application title

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -8,7 +8,7 @@ const LoginForm = () => {
   return (
     <div className='wrapper'>
       <div className='form'>
-        <h1 className='title'></h1>
+        <h1 className='title'>Chat Application</h1>
       </div>
     </div>
   );
