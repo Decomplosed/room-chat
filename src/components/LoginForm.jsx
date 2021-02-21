d16e8d72ad6afdf1308e95d30199387986764c6a
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-21 12:14:31 +0100

    passe user secret to auth object

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -12,6 +12,7 @@ const LoginForm = () => {
     const authObject = {
       'Project-ID': process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID,
       'User-Name': username,
+      'User-Secret': password,
     };
 
     try {
