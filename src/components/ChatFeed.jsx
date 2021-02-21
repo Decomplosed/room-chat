52b20d13b3ff7e1efe68d6062a5fa3f242effa17
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-21 12:20:34 +0100

    small amendments within chat feed component

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import MessageForm from './MessageForm';
 import MyMessage from './MyMessage';
 import TheirMessage from './TheirMessage';
+import MessageForm from './MessageForm';
 
 const ChatFeed = (props) => {
   const { chats, activeChat, userName, messages } = props;
+
   const chat = chats && chats[activeChat];
 
-  const renderReadReceipts = (message, isMyMessage) => {
-    return chat.people.map(
+  const renderReadReceipts = (message, isMyMessage) =>
+    chat.people.map(
       (person, index) =>
         person.last_read === message.id && (
           <div
@@ -16,12 +16,12 @@ const ChatFeed = (props) => {
             className='read-receipt'
             style={{
               float: isMyMessage ? 'right' : 'left',
-              backgroundImage: `url(${person?.person?.avatar})`,
+              backgroundImage:
+                person.person.avatar && `url(${person.person.avatar})`,
             }}
           />
         ),
     );
-  };
 
   const renderMessages = () => {
     const keys = Object.keys(messages);
@@ -32,7 +32,7 @@ const ChatFeed = (props) => {
       const isMyMessage = userName === message.sender.username;
 
       return (
-        <div key={`msg_${index}`}>
+        <div key={`msg_${index}`} style={{ width: '100%' }}>
           <div className='message-block'>
             {isMyMessage ? (
               <MyMessage message={message} />
@@ -57,12 +57,12 @@ const ChatFeed = (props) => {
     });
   };
 
-  if (!chat) return 'Loading...';
+  if (!chat) return <div />;
 
   return (
     <div className='chat-feed'>
       <div className='chat-title-container'>
-        <div className='chat-title'>{chat.title}</div>
+        <div className='chat-title'>{chat?.title}</div>
         <div className='chat-subtitle'>
           {chat.people.map((person) => ` ${person.person.username}`)}
         </div>
