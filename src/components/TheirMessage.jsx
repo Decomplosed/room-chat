e71d2bfa0e2f00fd5ba13d743914878e74a25b6a
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-21 12:22:02 +0100

    fixes within TheirMessage component

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -9,10 +9,12 @@ const TheirMessage = ({ lastMessage, message }) => {
       {isFirstMessageByUser && (
         <div
           className='message-avatar'
-          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+          style={{
+            backgroundImage: message.sender && `url(${message.sender.avatar})`,
+          }}
         />
       )}
-      {message?.attachments?.length > 0 ? (
+      {message.attachments && message.attachments.length > 0 ? (
         <img
           src={message.attachments[0].file}
           alt='message-attachment'
@@ -24,8 +26,8 @@ const TheirMessage = ({ lastMessage, message }) => {
           className='message'
           style={{
             float: 'left',
-            color: 'white',
             backgroundColor: '#CABCDC',
+            marginLeft: isFirstMessageByUser ? '4px' : '48px',
           }}
         >
           {message.text}
